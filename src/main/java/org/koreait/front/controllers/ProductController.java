1531aaaa690c4dd037032c49a34cbc01ec9abf75
seonmin1 <https://github.com/ai-team1project/Pokemon_Project.git>
2024-12-26 21:27:38 +0900

    .

--- src/main/java/org/koreait/front/controllers/ProductController.java
+++ src/main/java/org/koreait/front/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package org.koreait.front.controllers;
 
+import org.koreait.front.common.BaseModel;
 import org.koreait.front.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +13,15 @@ import java.util.List;
 public class ProductController {
 
     @GetMapping("/goods")
-    public String goods(Model model) {
+    public String product(Model model) {
 
-        List<Product>productList = new ArrayList<>();
+        List<BaseModel> productNewsList = new ArrayList<>();
+        productNewsList.add(new BaseModel("해피햅삐 잠만보 이모티콘 출시!!", "2024년 07월 31일", "https://data1.pokemonkorea.co.kr/newdata/2024/07/2024-07-31_12-01-55-44458-1722394915.png", "https://e.kakao.com/t/happy-happyyy-jammanbo?lang=ko&referer=co_mkt"));
+        model.addAttribute("productNewsList", productNewsList);
 
-        productList.add(new Product("", "", "", ""));
-        model.addAttribute("productList", productList);
+        List<Product>productGoodsList = new ArrayList<>();
+        productGoodsList.add(new Product("[성수미술관] 포켓몬 도안", "식품 및 기타", "https://data1.pokemonkorea.co.kr/newdata/2024/11/2024-11-21_13-47-51-31339-1732164471.jpg", "https://pokemonkorea.co.kr/goods/view/15455"));
+        model.addAttribute("productGoodsList", productGoodsList);
 
         return "front/product/product";
     }
