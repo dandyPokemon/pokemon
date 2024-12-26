8ea2d044c40845b4c059c85b85be68595379899c
rlaehddud475 <6070kjh475@naver.com>
2024-12-26 21:29:10 +0900

    .

--- src/main/java/org/koreait/front/controllers/GameController.java
+++ src/main/java/org/koreait/front/controllers/GameController.java
@@ -1,14 +1,30 @@
 package org.koreait.front.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.libs.Utils;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @RequestMapping("/game")
+@Controller
+@RequiredArgsConstructor
 public class GameController {
-    @GetMapping("/main")
-    public String game(Model model){
+    private final Utils utils;
 
-        return "game/game";
+    @GetMapping("/game")
+    public String game(Model model) {
+
+
+
+
+
+
+
+
+        // 템플릿 반환
+        return utils.tpl("game/game");
     }
 }
