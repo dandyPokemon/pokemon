3e16c9a9e7afd889a557d90c725aeffff242573b
rlaehddud475 <6070kjh475@naver.com>
2024-12-30 11:30:10 +0900

    파일 위치 변경

--- src/main/java/org/koreait/front/controllers/GameController.java
+++ src/main/java/org/koreait/front/controllers/GameController.java
@@ -1,4 +1,16 @@
 package org.koreait.front.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.libs.Utils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class GameController {
+    private final Utils utils;
+    @GetMapping("/game")
+    public String game(){
+        return utils.tpl("game/game");
+    }
 }
